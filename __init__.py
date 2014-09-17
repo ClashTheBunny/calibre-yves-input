e7b617aa83111f17fd79941d0358881bcf87d394
Randall Mason <randall@mason.ch>
2014-09-17 16:33:53 +0300

    move most of the logic out of the plugin to increase testablility

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # vim:fileencoding=utf-8
 from calibre.ptempfile import PersistentTemporaryDirectory
-import os, json
 
 __license__ = 'GPL v3'
 __copyright__ = '2014, Randall Mason <Randall@Mason.CH>'
 
+import os
 
 from calibre.ebooks.html.to_zip import HTML2ZIP
-from calibre_plugins.yves_input.yvesDecode import readFile
+from calibre_plugins.yves_input.yvesDecode import yvesDir2HTML
 
 class YVES2ZIP(HTML2ZIP):
 
@@ -23,52 +23,6 @@ class YVES2ZIP(HTML2ZIP):
 
     def run(self, yvesfile):
         yves_temp_directory = PersistentTemporaryDirectory('yves_input')
-
-        manifestJson = readFile(yvesfile)
-        # for iOS bibles, everything is stored in a plist, eventually use something like this:
-        # xmltodict.parse(plistDecode.plistFromString(base64.decodestring(xmltodict.parse(plistDecode.plistFromFile('tv.lifechurch.bible.plist','xml1'))['plist']['dict']['dict'][0]['data'][0]),'xml1'))
-
-        yvesDir = os.path.dirname(yvesfile)
-
-        bibleMetaData = json.loads(manifestJson)
-        bibleName = bibleMetaData['abbreviation'] + ".html"
-
-        DEST = open(os.path.join(yves_temp_directory, bibleName), 'w')
-        DEST.write( '<html dir="' + bibleMetaData['language']['text_direction'].encode('utf8') + '"><head><title>' )
-        DEST.write( bibleMetaData['local_title'].encode('utf8') )
-        DEST.write( '</title>\n')
-        if( bibleMetaData.has_key('publisher') & bibleMetaData['publisher'].has_key('name') & ( bibleMetaData['publisher']['name'] != None ) ):
-            DEST.write( '<meta name="Publisher" content="' + bibleMetaData['publisher']['name'].encode('utf8') + '">\n')
-        if( bibleMetaData.has_key('copyright_long') & bibleMetaData['copyright_long'].has_key('text') & ( bibleMetaData['copyright_long']['text'] != None ) ):
-            DEST.write( '<meta name="Copyright" content="' + bibleMetaData['copyright_long']['text'].encode('utf8') + '">\n')
-        if( bibleMetaData.has_key('language') & bibleMetaData['language'].has_key('iso_639_1') & ( bibleMetaData['language']['iso_639_1'] != None ) ):
-            DEST.write( '<meta name="DC.language" content="' + bibleMetaData['language']['iso_639_1'].encode('utf8') + '">\n')
-        elif( bibleMetaData.has_key('language') & bibleMetaData['language'].has_key('iso_639_3') & ( bibleMetaData['language']['iso_639_3'] != None ) ):
-            DEST.write( '<meta name="DC.language" content="' + bibleMetaData['language']['iso_639_3'].encode('utf8') + '">\n')
-        DEST.write( '<meta name="Source" content="YouVersion">\n')
-        DEST.write( '<style type="text/css">' )
-        DEST.write( '</style>' )
-        DEST.write( '</head><body>\n' )
-        for book in bibleMetaData['books']:
-            DEST.write('<div class="book">\n<div class="bookTitle">')
-            DEST.write(book['human_long'].encode('utf8'))
-            DEST.write('</div>\n')
-            for chapter in book['chapters']:
-                chapterFile = chapter['usfm'][len(book['usfm'])+1:]
-                # DEST.write('<a href="')
-                # DEST.write(book['usfm'])
-                # DEST.write("/")
-                # DEST.write(chapterFile)
-                # DEST.write('.html">')
-                # DEST.write(book['abbreviation'].encode('utf8'))
-                # DEST.write(' ')
-                # DEST.write(chapter['human'])
-                # DEST.write('</a>')
-                # DEST.write('\n<br />\n')
-                chapterLines = readFile( os.path.join(yvesDir,book['usfm'],chapterFile + ".yves")).splitlines()
-                DEST.writelines( chapterLines[2:len(chapterLines)-2] )
-            DEST.write('</div>\n')
-        DEST.write( '</body></html>\n' )
+        bibleName = yvesDir2HTML(yvesfile,yves_temp_directory)
 
         return HTML2ZIP.run(self, os.path.join( yves_temp_directory, bibleName ))
-
--- yvesDecode.py
+++ yvesDecode.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys
+import sys, os, json
 
 def readFile(paramFile):
     file = open(paramFile, 'r')
@@ -20,9 +20,60 @@ def readString(arrayOfByte):
       i += 2;
     return ''.join([ chr(x & 0xFF) for x in byteArray])
 
+def loadJson(string):
+    return json.loads(string)
+
+def yvesDir2HTML(yvesfile, yves_temp_directory):
+        # for iOS bibles, everything is stored in a plist, eventually use something like this:
+        # xmltodict.parse(plistDecode.plistFromString(base64.decodestring(xmltodict.parse(plistDecode.plistFromFile('tv.lifechurch.bible.plist','xml1'))['plist']['dict']['dict'][0]['data'][0]),'xml1'))
+        bibleMetaData = loadJson(readFile(yvesfile))
+
+        yvesDir = os.path.dirname(yvesfile)
+
+        bibleName = bibleMetaData['abbreviation'] + ".html"
+
+        DEST = open(os.path.join(yves_temp_directory, bibleName), 'w')
+        DEST.write( '<html dir="' + bibleMetaData['language']['text_direction'].encode('utf8') + '"><head><title>' )
+        DEST.write( bibleMetaData['local_title'].encode('utf8') )
+        DEST.write( '</title>\n')
+        if( bibleMetaData.has_key('publisher') and bibleMetaData['publisher'] and bibleMetaData['publisher'].has_key('name') and bibleMetaData['publisher']['name'] ):
+            DEST.write( '<meta name="Publisher" content="' + bibleMetaData['publisher']['name'].encode('utf8') + '">\n')
+        if( bibleMetaData.has_key('copyright_long') and bibleMetaData['copyright_long'].has_key('text') and bibleMetaData['copyright_long']['text'] ):
+            DEST.write( '<meta name="Copyright" content="' + bibleMetaData['copyright_long']['text'].encode('utf8') + '">\n')
+        if( bibleMetaData.has_key('language') and bibleMetaData['language'].has_key('iso_639_1') and bibleMetaData['language']['iso_639_1'] ):
+            DEST.write( '<meta name="DC.language" content="' + bibleMetaData['language']['iso_639_1'].encode('utf8') + '">\n')
+        elif( bibleMetaData.has_key('language') and bibleMetaData['language'].has_key('iso_639_3') and bibleMetaData['language']['iso_639_3'] ):
+            DEST.write( '<meta name="DC.language" content="' + bibleMetaData['language']['iso_639_3'].encode('utf8') + '">\n')
+        DEST.write( '<meta name="Source" content="YouVersion">\n')
+        DEST.write( '<style type="text/css">' )
+        DEST.write( '</style>' )
+        DEST.write( '</head><body>\n' )
+        for book in bibleMetaData['books']:
+            DEST.write('<div class="book">\n<div class="bookTitle">')
+            DEST.write(book['human_long'].encode('utf8'))
+            DEST.write('</div>\n')
+            for chapter in book['chapters']:
+                chapterFile = chapter['usfm'][len(book['usfm'])+1:]
+                # DEST.write('<a href="')
+                # DEST.write(book['usfm'])
+                # DEST.write("/")
+                # DEST.write(chapterFile)
+                # DEST.write('.html">')
+                # DEST.write(book['abbreviation'].encode('utf8'))
+                # DEST.write(' ')
+                # DEST.write(chapter['human'])
+                # DEST.write('</a>')
+                # DEST.write('\n<br />\n')
+                chapterLines = readFile( os.path.join(yvesDir,book['usfm'],chapterFile + ".yves")).splitlines()
+                DEST.writelines( chapterLines[2:len(chapterLines)-2] )
+            DEST.write('</div>\n')
+        DEST.write( '</body></html>\n' )
+
+        return bibleName
+
 if __name__ == '__main__':
     if len(sys.argv[1:]) > 0:
         for file in sys.argv[1:]:
-            print readFile(file)
+            print loadJson(readFile(file))
     else:
-        print readFile("manifest.yves")
+        print yvesDir2HTML("manifest.yves",".")
