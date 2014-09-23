0ed3c4765281bf0288b1debb137b65923dad6060
Randall Mason <randall@mason.ch>
2014-09-23 15:13:24 +0300

    convert to subclassing FileTypePlugin because older calibre don't work.  They claim HTML2ZIP is being duplicated.

--- __init__.py
+++ __init__.py
@@ -7,10 +7,11 @@ __copyright__ = '2014, Randall Mason <Randall@Mason.CH>'
 
 import os
 
+from calibre.customize import FileTypePlugin
 from calibre.ebooks.html.to_zip import HTML2ZIP
 from calibre_plugins.yves_input.yvesDecode import yvesDir2HTML
 
-class YVES2ZIP(HTML2ZIP):
+class YVES2ZIP(FileTypePlugin):
 
     name = 'YVES to ZIP'
     author = 'Randall Mason'
@@ -18,11 +19,12 @@ class YVES2ZIP(HTML2ZIP):
     supported_platforms = ['linux','osx','windows']
     file_types = set(['yves'])
     minimum_calibre_version = (2, 3, 0)
-    version = (0, 0, 6)
+    version = (0, 0, 7)
     on_import = True
 
     def run(self, yvesfile):
+        html2zip = HTML2ZIP(None)
         yves_temp_directory = PersistentTemporaryDirectory('yves_input')
         bibleName = yvesDir2HTML(yvesfile,yves_temp_directory)
 
-        return HTML2ZIP.run(self, os.path.join( yves_temp_directory, bibleName ))
+        return html2zip.run(os.path.join( yves_temp_directory, bibleName ))
